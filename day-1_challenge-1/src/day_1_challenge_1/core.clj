29538aca51e5aa7c228dfa595552fa16d42e61aa
DucoErgoSum <stephen.feyrer@btinternet.com>
2022-12-02 17:31:56 +0000

    [Day 1] Challlenge 1.  Highest calories with input.

--- day-1_challenge-1/src/day_1_challenge_1/core.clj
+++ day-1_challenge-1/src/day_1_challenge_1/core.clj
@@ -2,13 +2,8 @@
   (:gen-class))
 (require '[clojure.string :as str])
 
-(def elves (map #(str/split % #"\r?\n") (str/split (slurp "resources/calories.txt") #"\r?\n\r?\n")))
-(def totals (let [[Hughy Chesterfield Craston Marmite Tim] elves]
-              [(reduce + (map #(Integer. %) Hughy))
-                    (reduce + (map #(Integer. %) Chesterfield))
-                    (reduce + (map #(Integer. %) Craston))
-                    (reduce + (map #(Integer. %) Marmite))
-                    (reduce + (map #(Integer. %) Tim))]))
+(def elves (map #(str/split % #"\r?\n") (str/split (slurp "resources/input.txt") #"\r?\n\r?\n")))
+(def totals (mapv (fn [coll] (reduce + (map #(Integer. %) coll))) elves))
 (def highest-calories (apply max totals))
 
 (defn -main
